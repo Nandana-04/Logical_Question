85ffb3a1ea9449c8fdbf95889f0660c08ec6c841
Nandana-04 <91598452+Nandana-04@users.noreply.github.com>
2023-01-18 17:13:18 +0530

    Add files via upload

---
+++ Ques4.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter the Date of Birth :");
+var dob = DateTime.Parse(Console.ReadLine());
+
+
+var year = dob.Year;
+if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+{
+    Console.WriteLine("Leap year");
+}
+else
+{
+    Console.WriteLine("Not a leap year");
+}
+
+
+
+TimeSpan day = DateTime.Now - dob;
+Console.WriteLine("Age in Years :" + day.Days / 365);
+Console.WriteLine("age in Months :" + (day.Days / 365) * 12);
+Console.WriteLine("age in weeks : " + day.Days / 7);
+Console.WriteLine("age in Days : " + day.Days);
+Console.WriteLine("age in Hours : " + day.Days * 24);
+
+
+
+
---
+++ Ques7.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Enter the name of the passenger : ");
+string names=Console.ReadLine();
+
+Console.WriteLine("Enter total miles travelled : ");
+double total_miles = Convert.ToDouble(Console.ReadLine());
+
+if (total_miles > 10000 && total_miles < 20000)
+{
+    Console.WriteLine("{0} travelled {1} miles and Award 10 frequent flyer points " , names,total_miles );
+}
+else if (total_miles > 20000 && total_miles < 50000)
+{
+    Console.WriteLine("{0} travelled {1} miles and Award 20 frequent flyer points ", names, total_miles);
+}
+else if (total_miles > 50000 && total_miles < 100000)
+{
+    Console.WriteLine("{0} travelled {1} miles and Award 30 frequent flyer points ", names, total_miles);
+}
+else if(total_miles > 100000)
+{
+    Console.WriteLine("{0} travelled {1} miles and Award 50 frequent flyer points ", names, total_miles);
+}
+else
+{
+    Console.WriteLine("{0} travelled {1} miles and Nooo flyer points ", names, total_miles);
+}
