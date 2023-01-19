5c55af2a5d1c77c46919c410dad0ce11e47d9a57
Nandana-04 <91598452+Nandana-04@users.noreply.github.com>
2023-01-19 11:36:32 +0530

    Add files via upload

---
+++ Ques8.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace Flightfare
+{
+    internal class Class1
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter flight fares in decimal format of  passenger: ");
+            decimal[] fare = new decimal[5];
+            for (int i = 0; i < fare.Length; i++)
+            {
+                fare[i] = Convert.ToDecimal(Console.ReadLine());
+            }
+            Console.WriteLine("Enter destinations  of passenger: ");
+            string[] destination = new string[5];
+            for (int i = 0; i < destination.Length; i++)
+            {
+                destination[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Enter the index between 5 :");
+            int index = Convert.ToInt32(Console.ReadLine());
+
+
+            if (index < 5) 
+            {
+                Console.WriteLine("Destination :" + destination[index]);
+                Console.WriteLine("Fare :" + fare[index]);
+            
+            }
+            else
+            {
+                Console.WriteLine("Not Valid");
+            }
+
+
+        }
+    }
+}
