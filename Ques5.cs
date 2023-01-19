7f61b12a4983d85cd175827c384d41176ca0d10f
Nandana-04 <91598452+Nandana-04@users.noreply.github.com>
2023-01-19 10:23:27 +0530

    Add files via upload

---
+++ Ques5.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Enter the number of passenger: ");
+int pass = Convert.ToInt32(Console.ReadLine());
+decimal[] fare = new decimal[pass * 2];
+
+int eindex = 0; 
+int oindex = 1;
+
+Console.WriteLine("Enter the passenger :");
+for (int i = 0; i < pass; i++)
+{
+    decimal value = Convert.ToDecimal(Console.ReadLine());
+    if (value % 2 == 0)
+    {
+        fare[eindex] = value;
+        eindex += 2;
+    }
+    else
+    {
+        fare[oindex] = value;
+        oindex += 2;
+
+    }
+
+}
+
+foreach (var item in fare)
+{
+    Console.WriteLine("Array at even or odd positions :" );
+    Console.WriteLine(item);
+}
+
+
+
+
